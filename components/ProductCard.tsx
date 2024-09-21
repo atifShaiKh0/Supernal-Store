5c8b15b34b01518586f653ecf3a8e949351fc3a4
atifShaikh0 <maatif881@gamil.com>
2024-09-21 10:17:16 +0530

    fetching collections and products

---
+++ components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductCard = ({ product }: { product: ProductType }) => {
+  return (
+    <div>
+      Product Card
+      <h1></h1>
+    </div>
+  );
+};
+
+export default ProductCard;
